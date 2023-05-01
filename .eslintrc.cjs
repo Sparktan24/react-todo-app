7788342a077fb53765ece0e31921eb595b24219c
Jesus Molina <netod90@gmail.com>
2023-05-01 13:04:13 -0700

    Test lintern

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,6 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [],
   rules: {},
 };
